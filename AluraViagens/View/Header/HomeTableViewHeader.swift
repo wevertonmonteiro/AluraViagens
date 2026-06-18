518afacdbe12253b908d3b370f8257dbe7c7c746
WEVERTON COSME DE CASTRO MONTEIRO <weverton.monteiro+bradesco@bradesco.com.br>
2026-06-18 15:51:26 -0300

    Iniciei o arredondamento do header.

--- AluraViagens/View/Header/HomeTableViewHeader.swift
+++ AluraViagens/View/Header/HomeTableViewHeader.swift
@@ -17,7 +17,13 @@ class HomeTableViewHeader: UIView {
     
     @IBOutlet weak var bannerImageView: UIImageView!
     
+    @IBOutlet weak var bannerView: UIView!
     func configurarView() {
         headerView.backgroundColor = UIColor(red: 30.0/255, green: 59.0/255, blue: 119.0/255, alpha: 1)
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 100
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
 }
