ea4babca52c55dc91d00aefac815c9dea882f6d8
WEVERTON COSME DE CASTRO MONTEIRO <weverton.monteiro+bradesco@bradesco.com.br>
2026-06-19 10:15:58 -0300

    Criei a nova celula customizada.

---
+++ AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  
+//
+//  Created by Weverton Cosme de Castro Monteiro on 19/06/26.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
